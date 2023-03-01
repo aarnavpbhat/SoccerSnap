72a1df851c9333d1af6238871f38922ad34d983e
aarnavpbhat <109448062+aarnavpbhat@users.noreply.github.com>
2023-02-28 21:16:19 -0800

    Example code working!!!

--- flask-server-main/app.py
+++ flask-server-main/app.py
@@ -18,7 +18,7 @@ ASSETS_PATH = os.path.join(".", "./model")
 TF_MODEL = TFModel(ASSETS_PATH)
 
 
-@app.post('/predict')
+@app.get('/predict')
 def predict_image():
     req = request.get_json(force=True)
     image = _process_base64(req)
